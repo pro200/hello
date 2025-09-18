37790a6926eed7d03b392b874e82912622a46b5c
pro200 <pro200@gmail.com>
2025-09-18 16:06:45 +0900

    update

--- hello.go
+++
@@ -1,7 +0,0 @@
-package hello
-
-import "fmt"
-
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s~~! v1.0.7", name)
-}
---
+++ hellox.go
@@ -0,0 +1,7 @@
+package hellox
+
+import "fmt"
+
+func Hi(name string) string {
+	return fmt.Sprintf("Hix, %s~~! v1.0.7", name)
+}
